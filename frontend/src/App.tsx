2d8cb517674d9bc348f47510df65332e194874bd
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2025-02-01 09:28:29 +0530

    route: setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import BuyCredit from "./pages/BuyCredit";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import { PATHS } from "./constant";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path={PATHS.home} element={<Home />} />
+        <Route path={PATHS.result} element={<Result />} />
+        <Route path={PATHS.buyCredit} element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/constant.ts
@@ -0,0 +1,5 @@
+export const PATHS = {
+    home: "/",
+    result: "/result",
+    buyCredit: "/buy"
+}
