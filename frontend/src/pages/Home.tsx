d6b59330cf59da51eaad21e88f7fab29007806e7
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2025-02-01 13:21:47 +0530

    styled: descirption

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,15 @@
+import Description from "../components/Description";
 import Header from "../components/Header";
 import Steps from "../components/Steps";
+import Testimonials from "../components/Testimonials";
 
 function Home() {
   return (
     <div>
       <Header />
       <Steps />
+      <Description />
+      <Testimonials />
     </div>
   );
 }
